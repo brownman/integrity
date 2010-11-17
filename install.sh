5a119c17d7bb57548058d30a7bf031430534cdd6
Graham Christensen <graham@grahamc.com>
2010-11-17 15:03:43 -0500

    Updating the install.sh to be more complete and documented. Also split it up into functions for logical blocks. Start to finish install including load ballancer and mysql.

--- install.sh
+++ install.sh
@@ -7,40 +7,89 @@ echo "" > install/install.log
 echo "Performing local branch"
 git checkout -b deploy v22.3
 
-echo "Installing apt packages"
-sudo apt-get install ruby1.8-dev build-essential irb1.8 libreadline-ruby1.8\
-                     libreadline5 rdoc1.8 rubygems1.8 libsqlite3-dev sqlite3\
-                     libsqlite3-0 libxslt1-dev libxslt1.1 libsqlite3-dev\
-                     libxml2-dev nginx >> install/install.log
 
-echo "Performing manual gem update"
-echo "see: http://stackoverflow.com/questions/2777831"
-sudo gem install rubygems-update >> install/install.log
-sudo /var/lib/gems/1.8/bin/update_rubygems >> install/install.log
+# Install the Apt packages
+install_apt
 
-# Install the core gems
-echo "Installing gem: bundler"
-sudo gem install bundler >> install/install.log
-echo "Installing gem: thin"
-sudo gem install thin >> install/install.log
+# Manually update rubygems because Ubuntu blocks it
+install_gem_update
 
-# Install the Integrity gems in the Gemfile
-echo "Installing Integrity gems"
-sudo bundle install >> install/install.log
+# Install the Thin webserver (for Ruby)
+install_thin
 
-echo "Building the Integrity DB"
-rake db
+# Install the bundler gem
+install_bundler
 
-# Apply nginx site configuration
-compare_files "/etc/nginx/sites-available/default" "install/nginx_site_default"
-echo "Moving nginx site config into place"
-cp install/nginx_site_integrity /etc/nginx/sites-available/default >> install/install.log
+# Install the Integrity package and its gems
+install_integrity
 
-RANDOMSTRING=`random_string`
-echo "Your random string is: $RANDOMSTRING" >> install/install.log
-sed -ie "s/SECRETGITTOKEN/$RANDOMSTRING/" init.rb >> install/install.log
+# Install and configure nginx
+install_nginx
 
-echo "We are done here. Your GitHub token is: $RANDOMSTRING"
+# Setup the github token
+install_github_token
+
+
+
+# Install packages from Apt to support this system
+function install_apt {
+    echo "Installing apt packages"
+    sudo apt-get install ruby1.8-dev build-essential irb1.8 libreadline-ruby1.8\
+                         libreadline5 rdoc1.8 rubygems1.8 libsqlite3-dev sqlite3\
+                         libsqlite3-0 libxslt1-dev libxslt1.1 libsqlite3-dev\
+                         libxml2-dev nginx mysql-server-5.1 >> install/install.log
+}
+
+
+# Perform the manual update of the gem system
+function install_gem_update {
+    echo "Performing manual gem update"
+    echo "see: http://stackoverflow.com/questions/2777831"
+    sudo gem install rubygems-update >> install/install.log
+    sudo /var/lib/gems/1.8/bin/update_rubygems >> install/install.log
+}
+
+
+# Install the Thin Ruby webserver and the configuration
+function install_thin {
+    echo "Installing gem: thin"
+    sudo gem install thin >> install/install.log
+    sudo thin install >> install/install.log
+    sudo /usr/sbin/update-rc.d -f thin defaults >> instal/install.log
+    sudo cp install/thin.yml /etc/thin/integrity.yml >> install/install.log
+}
+
+# Install the Bundler gem for running and preparing Integrity
+function install_bundler {
+    echo "Installing gem: bundler"
+    sudo gem install bundler >> install/install.log
+}
+
+# Install Integrity's gems, lock the code, and then setup the database
+function install_integrity {
+    # Prepare Integrity
+    echo "Installing Integrity gems"
+    sudo bundle install >> install/install.log
+    echo "Building the Integrity DB"
+    rake db
+}
+
+# Install the nginx configuration
+function install_nginx {
+    # Apply nginx site configuration
+    compare_files "/etc/nginx/sites-available/default" "install/nginx_site_default"
+    echo "Moving nginx site config into place"
+    sudo cp install/nginx_site_integrity /etc/nginx/sites-available/default >> install/install.log
+}
+
+
+# Replace the secret github token in the init.rb script
+function install_github_token {
+    RANDOMSTRING=`random_string`
+    echo "Your random string is: $RANDOMSTRING" >> install/install.log
+    sed -ie "s/SECRETGITTOKEN/$RANDOMSTRING/" init.rb >> install/install.log
+    echo "Your random GitHub token is: $RANDOMSTRING"
+}
 
 # Create a random MD5 sum
 function random_string {
